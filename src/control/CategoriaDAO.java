aef84a3271f1c3df5cda8b36efa7aac4db69ec69
FrancescoDiPalma97 <52467756+FrancescoDiPalma97@users.noreply.github.com>
2020-05-11 19:24:30 +0200

    Update CategoriaDAO.java

--- src/control/CategoriaDAO.java
+++ src/control/CategoriaDAO.java
@@ -1,4 +1,56 @@
-package control;
+package controller;
+
+import model.Categoria;
+import model.ConPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CategoriaDAO {
+
+    public ArrayList<Categoria> retriveAll() {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("SELECT id, nome, descrizione FROM categoria");
+            ArrayList<Categoria> list = new ArrayList<>();
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Categoria c = new Categoria();
+                c.setId(rs.getInt(1));
+                c.setNome(rs.getString(2));
+                c.setDescrizione(rs.getString(3));
+                list.add(c);
+            }
+
+            return list;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Categoria getCategoria(String category) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("SELECT id, nome, descrizione FROM categoria WHERE nome=? ");
+
+            ps.setString(1,category);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Categoria c = new Categoria();
+                c.setId(rs.getInt(1));
+                c.setNome(rs.getString(2));
+                c.setDescrizione(rs.getString(3));
+                return c;
+            }
+            return null;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
