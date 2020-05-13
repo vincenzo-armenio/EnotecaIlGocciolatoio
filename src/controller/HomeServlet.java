99940841331fc2b266b398f674e9a91f1b5aca45
FrancescoDiPalma97 <52467756+FrancescoDiPalma97@users.noreply.github.com>
2020-05-13 14:34:36 +0200

    Update HomeServlet.java

--- src/controller/HomeServlet.java
+++ src/controller/HomeServlet.java
@@ -1,4 +1,33 @@
 package controller;
 
-public class HomeServlet {
+import model.Categoria;
+import model.Prodotto;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/index.html")
+public class HomeServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ProdottoDAO proDAO=new ProdottoDAO();
+        List<Prodotto> prodotti=proDAO.retriveAll();
+        getServletContext().setAttribute("prodotti", prodotti);
+
+        String address;
+        address = "/Home.jsp";
+        RequestDispatcher dispatcher =
+                request.getRequestDispatcher(address);
+        dispatcher.forward(request, response);
+
+    }
 }
