8856039e4652b54b55de3b202f5630cd41b0bec3
FrancescoDiPalma97 <52467756+FrancescoDiPalma97@users.noreply.github.com>
2020-05-19 13:33:35 +0200

    Update HomeServlet.java

--- src/controller/HomeServlet.java
+++ src/controller/HomeServlet.java
@@ -11,7 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @WebServlet("/index.html")
 public class HomeServlet extends HttpServlet {
@@ -20,8 +22,39 @@ public class HomeServlet extends HttpServlet {
             throws ServletException, IOException {
 
         ProdottoDAO proDAO=new ProdottoDAO();
-        List<Prodotto> prodotti=proDAO.retriveAll();
-        getServletContext().setAttribute("prodotti", prodotti);
+        /*
+
+            Prodotto p=new Prodotto();
+        double n = (int)(Math.random()*10);
+        p=proDAO.retriveOne((int) n);
+        getServletContext().setAttribute("prodotto", p);
+        }*/
+
+        Prodotto p1,p2,p3,p4,p5,p6,p7,p8;
+
+        p1 = proDAO.retriveOne((int) (Math.random() * 10));
+        getServletContext().setAttribute("p1" , p1);
+
+        p2 = proDAO.retriveOne((int) (Math.random() * 10));
+        getServletContext().setAttribute("p2" , p2);
+
+        p3 = proDAO.retriveOne((int) (Math.random() * 10));
+        getServletContext().setAttribute("p3" , p3);
+
+        p4 = proDAO.retriveOne((int) (Math.random() * 10));
+        getServletContext().setAttribute("p4" , p4);
+
+        p5 = proDAO.retriveOne((int) (Math.random() * 10));
+        getServletContext().setAttribute("p5" , p5);
+
+        p6 = proDAO.retriveOne((int) (Math.random() * 10));
+        getServletContext().setAttribute("p6" , p6);
+
+        p7 = proDAO.retriveOne((int) (Math.random() * 10));
+        getServletContext().setAttribute("p7" , p7);
+
+        p8 = proDAO.retriveOne((int) (Math.random() * 10));
+        getServletContext().setAttribute("p8" , p8);
 
         String address;
         address = "/Home.jsp";
@@ -31,3 +64,4 @@ public class HomeServlet extends HttpServlet {
 
     }
 }
+
