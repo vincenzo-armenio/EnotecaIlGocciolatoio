657ee4145a138deca813aaa5edac7c488d2ab3bc
FrancescoDiPalma97 <52467756+FrancescoDiPalma97@users.noreply.github.com>
2020-05-20 16:21:48 +0200

    Update MostraProdotto.java

--- src/controller/MostraProdotto.java
+++ src/controller/MostraProdotto.java
@@ -1,4 +1,31 @@
 package controller;
 
-public class MostraProdotto {
+import model.Prodotto;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/show-product")
+public class MostraProdotto extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String product=request.getParameter("product");
+        ProdottoDAO proDAO = new ProdottoDAO();
+        String address = null;
+
+        Prodotto p=proDAO.retriveByName(product);
+        request.setAttribute("prodotto", p);
+
+        address = "/Prodotto.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(address);
+        dispatcher.forward(request, response);
+
+    }
 }
