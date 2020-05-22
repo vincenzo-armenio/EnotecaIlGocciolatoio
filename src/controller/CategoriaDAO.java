31e10f9f2196c09aa84ed49bbbf6f0d3c28bc896
FrancescoDiPalma97 <52467756+FrancescoDiPalma97@users.noreply.github.com>
2020-05-22 19:13:30 +0200

    Add files via upload
    Aggiunta crittografia e diverse Query

--- src/controller/CategoriaDAO.java
+++ src/controller/CategoriaDAO.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CategoriaDAO {
-    
+
     public ArrayList<Categoria> retriveAll() {
         try (Connection con = ConPool.getConnection()) {
             PreparedStatement ps =
@@ -54,3 +54,4 @@ public class CategoriaDAO {
         }
     }
 }
+
