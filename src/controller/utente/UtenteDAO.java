a347a1fa4a8e4200d1ce3cbcf059d952b78c730d
FrancescoDiPalma97 <52467756+FrancescoDiPalma97@users.noreply.github.com>
2020-05-24 13:44:54 +0200

    Add files via upload
    Modificati i metodi e aggiunte varie correzioni

--- src/controller/utente/UtenteDAO.java
+++ src/controller/utente/UtenteDAO.java
@@ -1,36 +1,93 @@
-package controller.utente;
+package controller;
+
 import model.ConPool;
+import model.Prodotto;
 import model.Utente;
 
-import java.io.Serializable;
 import java.sql.*;
+import java.util.ArrayList;
 
-public class UtenteDAO implements Serializable {
+public class UtenteDAO {
+    public ArrayList<Utente> retriveAll() {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("SELECT email, username, ruolo FROM utente");
+            ArrayList<Utente> list = new ArrayList<>();
+            ResultSet rs = ps.executeQuery();
 
-
-    public static Utente RetriveByEmailPass(String email, String password) throws SQLException,
-            ClassNotFoundException {
-     try (Connection con = ConPool.getConnection()) {
-        PreparedStatement ps =
-                con.prepareStatement("SELECT email,password FROM Utente WHERE email=?");
-        ps.setString(1, email);
-        ps.setString( 2,password);
-        ResultSet rs = ps.executeQuery();
-
-        if (rs.next()) {
-            Utente u = new Utente();
-            u.setEmail(rs.getString(2));
-            u.setPassword(rs.getString(3));
-
-            return u;
+            while (rs.next()) {
+                Utente u = new Utente();
+                u.setEmail(rs.getString(1));
+                u.setUsername(rs.getString(2));
+                u.setRuolo(rs.getString(3));
+                list.add(u);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return null;
-    } catch (SQLException e) {
-        throw new RuntimeException(e);
     }
+
+    public ArrayList<String> retriveUser(){
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("SELECT username FROM utente ");
+
+            ResultSet rs = ps.executeQuery();
+
+            ArrayList<String> list = new ArrayList<>();
+
+            while (rs.next()) {
+                String e = rs.getString(1);
+                list.add(e);
+            }
+            return list;
+        }catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public Utente doRetrieveByUsernamePassword(String username, String password) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "SELECT id, username, pass, email, ruolo, accesso FROM utente WHERE username=? AND pass=SHA1(?)");
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Utente p = new Utente();
+                p.setId(rs.getInt(1));
+                p.setUsername(rs.getString(2));
+                p.setPass(rs.getString(3));
+                p.setEmail(rs.getString(4));
+                p.setRuolo(rs.getString(5));
+                p.setAccesso(rs.getBoolean(6));
+                return p;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doSave(Utente user) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO utente (email, username, pass) VALUES(?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getUsername());
+            ps.setString(3, user.getPass());
+            if (ps.executeUpdate() != 1) {
+                throw new RuntimeException("INSERT error.");
+            }
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            int id=rs.getInt(1);
+            user.setId(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
-
-
-
-
