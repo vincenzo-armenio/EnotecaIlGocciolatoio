0a8ca4a11a4f64caf63530ff14b68cc300b75992
FrancescoDiPalma97 <52467756+FrancescoDiPalma97@users.noreply.github.com>
2020-05-24 14:53:36 +0200

    Add files via upload
    Ora è possibile uscire dal sistema

--- src/controller/utente/Logout.java
+++ src/controller/utente/Logout.java
@@ -1,27 +1,40 @@
-package controller.utente;
+package controller;
 
 import java.io.IOException;
-
-import javax.servlet.*;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+/**
+ * @author Mattia De Rosa
+ *
+ */
 @WebServlet("/logout")
 public class Logout extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    public Logout() {
-        super();
-    }
-
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.removeAttribute("user");
+        request.getSession().removeAttribute("utente");
+        String address;
 
-            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
-            dispatcher.forward(request, response);
-        }
+        address = "index.html";
+        response.sendRedirect(address);
     }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
 }
